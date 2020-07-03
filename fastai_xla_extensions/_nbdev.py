0bf2ab9fa4f30dd215f0130e64c3a135b9615f50
Butch Landingin <butchland@gmail.com>
2020-07-03 20:13:06 +0800

    Update docs

--- fastai_xla_extensions/_nbdev.py
+++ fastai_xla_extensions/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"XLAOptFuncWrapper": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://butchland.github.io/fastai_xla_extensions/"
 
