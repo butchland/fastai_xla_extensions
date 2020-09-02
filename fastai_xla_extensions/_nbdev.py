27a8721f83f2929cf7b0c8884323f5963473ce8a
tyoc213 <tyoc213@u>
2020-09-01 19:16:14 -0500

    using `to_device`

--- fastai_xla_extensions/_nbdev.py
+++ fastai_xla_extensions/_nbdev.py
@@ -4,7 +4,6 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"XLA_AVAILABLE": "00_core.ipynb",
          "XLAOptimProxy": "00_core.ipynb",
-         "set2xla_device": "00_core.ipynb",
          "XLAOptCallback": "00_core.ipynb"}
 
 modules = ["core.py"]
