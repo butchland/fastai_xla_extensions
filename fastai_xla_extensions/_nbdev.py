969117ea794a418963c3fdb994ed520ce8804ccd
Butch Landingin <butchland@gmail.com>
2021-01-12 00:43:42 +0800

    Add experimental module for running cifar10 dataset in fastai

--- fastai_xla_extensions/_nbdev.py
+++ fastai_xla_extensions/_nbdev.py
@@ -5,7 +5,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"IN_COLAB": "00_core.ipynb",
          "DEBUG": "00_core.ipynb",
          "TRACE": "00_core.ipynb",
-         "xla_imported": "00_core.ipynb",
+         "xla_imported": "01_utils.ipynb",
          "xla_available_config": "00_core.ipynb",
          "xla_module_exist": "00_core.ipynb",
          "__getstate__": "00_core.ipynb",
@@ -20,10 +20,29 @@ index = {"IN_COLAB": "00_core.ipynb",
          "XLAOptCallback": "00_core.ipynb",
          "Learner.to_xla": "00_core.ipynb",
          "Learner.detach_xla": "00_core.ipynb",
-         "print_aten_ops": "01_utils.ipynb"}
+         "print_aten_ops": "01_utils.ipynb",
+         "download_cifar10_dsets": "02_cifar_loader.ipynb",
+         "load_cifar_items": "02_cifar_loader.ipynb",
+         "load_classes": "02_cifar_loader.ipynb",
+         "CifarNP2ImageTransform": "02_cifar_loader.ipynb",
+         "Int2TensorTransform": "02_cifar_loader.ipynb",
+         "CifarImageTransform": "02_cifar_loader.ipynb",
+         "CifarImage2FloatTransform": "02_cifar_loader.ipynb",
+         "make_torch_tfms": "02_cifar_loader.ipynb",
+         "CifarTupleTransform": "02_cifar_loader.ipynb",
+         "TupleTorchDS": "02_cifar_loader.ipynb",
+         "make_cifar_item_tfm": "02_cifar_loader.ipynb",
+         "i2t_tfm": "02_cifar_loader.ipynb",
+         "cfnp2img_tfm": "02_cifar_loader.ipynb",
+         "cfimg_tfm": "02_cifar_loader.ipynb",
+         "cfimg2float_tfm": "02_cifar_loader.ipynb",
+         "make_cifar_tls": "02_cifar_loader.ipynb",
+         "make_cifar_dl": "02_cifar_loader.ipynb",
+         "make_fastai_cifar_dls": "02_cifar_loader.ipynb"}
 
 modules = ["core.py",
-           "utils.py"]
+           "utils.py",
+           "cifar_loader.py"]
 
 doc_url = "https://butchland.github.io/fastai_xla_extensions/"
 
