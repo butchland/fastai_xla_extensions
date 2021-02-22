3324becdd440b9c74aa513881b33899835d6aa18
Butch Landingin <butchland@gmail.com>
2021-02-22 22:39:09 +0800

    Add multi_core.callback and multi_core.lr_find to .all module

--- fastai_xla_extensions/all.py
+++ fastai_xla_extensions/all.py
@@ -3,4 +3,7 @@ from .misc_utils import *
 from .multi_core.base import *
 from .multi_core.torch_compat import *
 from .multi_core.learner import *
+from .multi_core.callback import *
+from .multi_core.lr_find import *
+
 
